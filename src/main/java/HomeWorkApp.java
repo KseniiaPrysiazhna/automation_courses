7f403ada8e941fbce02fc5ba9e6caaedd50f79c5
Kseniia Prysiazhna <kseniya.hens@gmail.com>
2022-04-09 11:31:11 +0300

    Add printTreeWords.

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,10 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        String name = "Orange\nBanana\nApple";
+        System.out.println(name);
+    }
+}
