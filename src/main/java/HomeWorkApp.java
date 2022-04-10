88439d3990a7485f4a313e7e3a51db036d8299bb
Kseniia Prysiazhna <kseniya.hens@gmail.com>
2022-04-10 17:55:05 +0300

    Add Homework2

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,46 +1,18 @@
 public class HomeWorkApp {
-    public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        checkColor();
-        compareNumbers();
-    }
-
-    public static void printThreeWords() {
-        String name = "Orange\nBanana\nApple";
-        System.out.println(name);
-    }
-
-    public static void checkSumSign() {
-        int a = 5;
-        int b = 3;
-        int x = a + b;
-        System.out.println(x);
-        if (x >= 0) {
-            System.out.println("Sum is positive");
+    public static boolean checkSum(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
         } else {
-            System.out.println("Sum is negative");
+            return false;
         }
     }
 
-    public static void checkColor() {
-        int value = 1;
-        if (value <= 0) {
-            System.out.println("Red");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Yellow");
-        } else if (value > 100) {
-            System.out.println("Green");
-        }
-    }
-
-    public static void compareNumbers() {
-        int a = 3636636;
-        int b = 188882;
-        if (a >= b) {
-            System.out.println("a>=b");
+    public static void checkNumber(int a) {
+        if (a >= 0) {
+            System.out.println("It is possitive");
         } else {
-            System.out.println("a<b");
+            System.out.println("It is negative");
         }
     }
 }
